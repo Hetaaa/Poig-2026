ca283a9f349adc12c282d52f1cfe07712aba7957
JakubBlaszczyk <jakub.blaszczyk@ardium.pl>
2026-05-25 23:49:41 +0200

    pr fixes mandatory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { Navbar } from "./features/Navbar/Navbar";
 import { Sidebar } from "./features/Sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 import "./App.scss";
-import "@fontsource/lato/400.css"; // Regular
-import "@fontsource/lato/700.css";
 
 export default function App() {
   return (
@@ -12,7 +10,7 @@ export default function App() {
       <div className="app-content">
         <Sidebar />
         <main className="app-main">
-          <Outlet/>
+          <Outlet />
         </main>
       </div>
     </>
